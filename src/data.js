a07596875b3f1c02f62bd5efb33733cc5176e3b0
Barath-K-R <132824019+Barath-K-R@users.noreply.github.com>
2023-08-02 13:01:19 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,33 @@
+const questions = [
+  {
+    id: 1,
+    title: 'How to write our name?',
+    info:
+      'you have to enter your name as per your name in aadhar card/Pan card',
+  },
+  {
+    id: 2,
+    title: 'How do I change my My Page password?',
+    info:
+      'Click forget password and try changing your password',
+  },
+  {
+    id: 3,
+    title: 'How can I apply for this job?',
+    info:
+      'Go to the official website of the company.Search for the job that meet the requirements of the recruiter and apply',
+  },
+  {
+    id: 4,
+    title: 'Who can apply for this job?',
+    info:
+      'One who have 5 years of experience in Frontend developing field',
+  },
+  {
+    id: 5,
+    title: 'How long have to wait after the submission of the appication?',
+    info:
+      'It does not take much time,our recruiting team analyses your resume and see whether you are eligible and send the mail',
+  },
+]
+export default questions
